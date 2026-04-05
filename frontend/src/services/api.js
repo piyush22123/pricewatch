13134837589f978caec71217549b8c71ebc25faf
aimerse-piyush-malviya <piyush.malviya@aimerse.in>
2026-04-05 11:41:37 +0530

    api url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://pricewatch-tx1n.onrender.com/api"
 });
 
 // Add token automatically
